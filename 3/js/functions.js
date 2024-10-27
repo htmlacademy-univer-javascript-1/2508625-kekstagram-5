622048ef2b66fcb741ce5a5aefbd517818e00c05
Keks <keks@htmlacademy.ru>
2024-10-27 16:35:28 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,19 @@
+const checkLength = (string = '', maxLength = 1) => (string.length <= maxLength);
+
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
+
+function checkPalindrome(string = '') {
+  const normalString = string.replaceAll(' ', '').toUpperCase();
+  let reversedString = '';
+
+  for (let i = normalString.length - 1; i >= 0; i--) {
+    reversedString += normalString[i];
+  }
+  return normalString === reversedString;
+}
+
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
